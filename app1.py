a686ad4b37574eabac8d24628a5cb340354485cc
ShamilaMim <58168466+ShamilaMim@users.noreply.github.com>
2020-09-08 17:02:14 +0600

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,4 +1,6 @@
 from tkinter import *
+
+#-----------------------------function----------------------------
 def increase():
     a= int(shamila1["text"]) #shamila1["text"] = "0"
     shamila1["text"] = f"{a+1}"
@@ -6,41 +8,26 @@ def increase():
 def decrease():
     a= int(shamila1["text"])
     shamila1["text"] = f"{a-1}"
-
     
-
-
-    
-
-
-
-    
-app = Tk()
+#------------------------------Screen----------------------------  
+#create a window or screen
+#create a Tk class object name "app"
+app = Tk() 
 app.title("firstapp")
 app.geometry("200x100")
 
+#-----------------------Button & Label-----------------------------
+#create a object shamila for "-" Button
 shamila = Button(app, text = "-", height= 2, width= 5, command = decrease )
 shamila.grid(row = 0,column = 0)
 
+#create a object shamila1 for Label
 shamila1 = Label(app, text = "0", fg = "red")
 shamila1.grid(row = 0, column = 1)
 
+#create a object shamila2 for "+" Button
 shamila2 = Button(app, text = "+", height= 2, width= 5, command = increase)
 shamila2.grid(row=0, column= 2)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 app.mainloop() #object.method()
 
